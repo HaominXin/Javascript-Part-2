c2b7c9bde16375d52d19c8d049421a792a0019a5
Alfred.xin <haomin.working@gmail.com>
2022-12-16 17:42:12 +1000

    part 2

--- script.js
+++ script.js
@@ -29,8 +29,78 @@
         b: 4,
         c: 5
     }
-    const returnTarget = Object.assign(source, target);
-    console.log("returnTarget:", returnTarget, typeof returnTarget);
+    const returnTarget = Object.assign(target, source);
+    // const newReturnTarget = Object.assign(source, target);
+    console.log("returnTarget:", returnTarget);
+    // console.log("newReturnTarget:", newReturnTarget);
     console.log("target:", target);
     console.log("source:", source);
 }
+
+{
+    console.clear();
+    const target = {
+        a: 1,
+        b: 2,
+    }
+    const source = {
+        b: 4,
+        c: 5
+    }
+    const combineTarget = {...target, ...source};
+    console.log("target:", target);
+    console.log("source:", source);
+    const returnTarget = Object.assign(target, source);
+    console.log("target:", target);
+    console.log("source:", source);
+    console.log("combineTarget:", combineTarget);
+    console.log("returnTarget:", returnTarget);
+
+    const obj = new Object({name: "alfred"});
+    console.log("obj:")
+}
+
+{
+    console.clear();
+    const arr = [{
+        name: "Alfred",
+        age: 22,
+        profile: {
+            address: "Toowong",
+        }
+    }]
+    const newArr = [{...arr[0]}];
+    newArr[0].name = "Haomin";
+    newArr[0].profile.address = "Brisbane"
+    console.log("arr:", arr);
+    console.log("newArr:", newArr);
+}
+
+// {
+//     // 下次上课询问关于array怎么处理
+//     console.clear();
+//     let _ = require("lodash")
+//     const user = {
+//         name: "Alfred",
+//         age: 22,
+//         profile: {
+//             address: "Queensland",
+//             email: "gmail.com"
+            
+//         }
+//     }
+//     const newUser = {...user}
+//     const newCloneUser = _.cloneDeep(user);
+    
+//     newUser.age = 18;
+//     newUser.profile.email = "@gmail.com";
+//     console.log("user:", user);
+//     console.log("newUser:", newUser);
+    
+//     console.clear();
+//     newCloneUser.age = 20;
+//     newCloneUser.profile.email = "alfred.xin@gmail.com";
+//     console.log("user:", user);
+//     console.log("newCloneUser:", newCloneUser);
+// }
+
